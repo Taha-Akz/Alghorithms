182d8554236f6612e4b4b1c859081f27526f77b4
Taha Akbarzadeh <tahaakz1387@gmail.com>
2026-01-03 18:32:55 +0330

    Sorting Array in C#

---
+++ SortingArray/Program.cs
@@ -0,0 +1,20 @@
+﻿ int  []  sort (params int [] arr)
+{
+
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = 0; j < arr.Length - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+int [] sortedNum = sort (8, 77,888,7,41);
