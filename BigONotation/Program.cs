0c688f58f08081aada801cb57ed67a5af061d816
Taha Akbarzadeh <tahaakz1387@gmail.com>
2026-01-05 17:08:40 +0330

    Big O notation

--- BigONotation/Program.cs
+++ BigONotation/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void log (int [] num)
+{
+    // O(n)
+    for (int i = 0; i < num.lench; i++)
+    {
+        System.Console.WriteLine(num[i]);
+
+    }
+}
+void log(int[] num)
+{
+    // RuntimeComlexity = O(n) + O(1)+ O(1) ==> O(n) When we have an
+    // Linear (it means Bu count of inputs it changes)  runtime complexity how much our inputs become more that much change
+    System.Console.WriteLine("j");
+    for (int i = 0; i < num.lench; i++)
+    {
+        System.Console.WriteLine(num[i]);
+
+    }
+    System.Console.WriteLine("d");
+}
+
+void SayHello ()
+{
+    //O (1)
+    System.Console.WriteLine("Hello Taha ! ");
+}
